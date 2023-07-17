2178cbc2ecc7ad00af1ff40f383958ba8addef28
koniiing <swanyjke@gmail.com>
2023-07-17 21:40:45 +0900

    4주차 고은+세환 폴더 이동

--- 4주차/세환/3-10.js
+++
@@ -1,9 +0,0 @@
-/* Api 호출 */
-// fetch : api 호출 도와주는 내장함수 , 비동기 처리 -> then으로 처리 가능
-async function getData() {
-  let rawResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
-  let jsonResponse = await rawResponse.json();
-  console.log(jsonResponse);
-}
-
-getData();
